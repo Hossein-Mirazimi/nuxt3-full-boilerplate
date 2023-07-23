79e0ea16b5e7db765c398a52de8949fb11b34b82
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2023-07-23 19:06:51 +0330

    feat: inital project

---
+++ components/global/LanguageSelector.vue
@@ -0,0 +1,20 @@
+<script setup>
+const { locale, availableLocales } = useI18n();
+</script>
+<template>
+  <form>
+    <label for="languages">
+      <Icon name="prime:language" size="1.5em" />
+    </label>
+    <select v-model="locale" class="bg-transparent">
+      <option
+        v-for="language in availableLocales"
+        :key="language"
+        :value="language"
+        :selected="locale === language"
+      >
+        {{ $t("locale." + language) }}
+      </option>
+    </select>
+  </form>
+</template>
